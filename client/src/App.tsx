a64fc5abffaec7e7b6d9f9c8df4583efd421028c
Frances Fogarty <53113092+femmiennine@users.noreply.github.com>
2022-11-02 14:11:56 +0100

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>FS React Node Typescript Boilerplate</h1>
+      <p>with ESLINT and PRETTIER</p>
+    </div>
+  )
+}
+
+export default App
